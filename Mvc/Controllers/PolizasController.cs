428326f6d267c61c36620b0955213875384f272c
jairotabaresh <jairotabares@gmail.com>
2018-09-29 09:39:56 -0500

    Se añade el controlador y el modelo al proyecto MVC, se crea una clase Globals para poder consumir el servicio inicial de listado de pólizas y se lista en pantalla las pólizas usando MVC

---
+++ Mvc/Controllers/PolizasController.cs
@@ -0,0 +1,22 @@
+﻿using Mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc.Controllers
+{
+    public class PolizasController : Controller
+    {
+        // GET: Polizas
+        public ActionResult Index()
+        {
+            IEnumerable<mvcPolizasModel> poliList;
+            HttpResponseMessage response = Globals.webApiClient.GetAsync("Polizas").Result;
+            poliList = response.Content.ReadAsAsync<IEnumerable<mvcPolizasModel>>().Result;
+            return View(poliList);
+        }
+    }
+}
---
+++ Mvc/Models/mvcPolizasModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc.Models
+{
+    public class mvcPolizasModel
+    {
+        public int idPoliza { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string tipoCubrimiento { get; set; }
+        public Nullable<int> Cobertura { get; set; }
+        public Nullable<System.DateTime> inicioVigencia { get; set; }
+        public Nullable<int> periodoCobertura { get; set; }
+        public Nullable<decimal> Precio { get; set; }
+        public string tipoRiesgo { get; set; }
+    }
+}
